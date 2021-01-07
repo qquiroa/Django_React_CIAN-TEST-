319c1eb5a028445d8593d5110f3f48d53f57e60e
qquiroa <qquiroa099@gmail.com>
2021-01-07 08:31:28 -0600

    IMPLEMENT BASIC TEMPLATE AND START REDUX

---
+++ frontend/src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react'
+import ReactDOM from 'react-dom'
+
+import Header from './layout/Header'
+
+import { Provider } from 'react-redux'
+import store from '../store'
+
+class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <Fragment>
+                    <Header />
+                </Fragment>
+            </Provider>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
